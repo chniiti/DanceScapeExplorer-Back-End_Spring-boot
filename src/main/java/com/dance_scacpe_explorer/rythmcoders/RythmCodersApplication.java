fd2be0fe9abc79b979facbd62cd0b77b2882917f
MedKhoualdia <92829701+MedKhoualdia@users.noreply.github.com>
2024-02-28 14:41:11 +0100

    V5

--- src/main/java/com/dance_scacpe_explorer/rythmcoders/RythmCodersApplication.java
+++ src/main/java/com/dance_scacpe_explorer/rythmcoders/RythmCodersApplication.java
@@ -3,7 +3,7 @@ package com.dance_scacpe_explorer.rythmcoders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class RythmCodersApplication {
